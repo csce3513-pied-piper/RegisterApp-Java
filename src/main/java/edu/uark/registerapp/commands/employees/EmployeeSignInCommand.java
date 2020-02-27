1dec18a93c93d1e7352f3cd822aaf090e0cda524
thewa <wjdunn@uark.edu>
2020-02-27 17:32:03 -0600

    Added three controller classes: ActiveEmployeeExistsQuery, ActiveUserDeleteCommand, EmployeeSignInCommand

---
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -0,0 +1,109 @@
+package edu.uark.registerapp.commands.employees;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.commands.products.ProductByLookupCodeQuery;
+import edu.uark.registerapp.models.api.Employee;
+import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.entities.ProductEntity;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+
+public class EmployeeSignInCommand implements VoidCommandInterface{
+	@Override
+	public void execute() {
+		//Validate EmployeeSignIn object
+		this.validateProperties();
+		
+		//Query for the employee using employee id
+		final Optional<EmployeeEntity> employeeEntity =
+				this.employeeRepository.findByEmployeeId(Integer.parseInt(this.employeeSignIn.employeeId));
+		
+		//Convert passwords to byte arrays
+		byte[] requestPassword = this.employeeSignIn.password.getBytes();
+		byte[] databasePassword = employeeEntity.get().getPassword();
+		
+		//Verify employee exists and passwords match
+		if (employeeEntity.isPresent()) {
+			if(Arrays.equals(requestPassword, databasePassword))
+				this.updateActiveEmployee();
+			else 
+				throw new NotFoundException("Employee"); 		
+		} 
+		else 
+			throw new NotFoundException("Product");
+		
+	}
+	
+	
+	// Helper methods
+	private void validateProperties() {
+		//Check for a blank or non-numeric employee id
+		if(StringUtils.isBlank(this.employeeSignIn.employeeId) || StringUtils.isNumeric(this.employeeSignIn.employeeId))
+			throw new UnprocessableEntityException("Employee ID");
+		
+		//Check for a blank password
+		if(!StringUtils.isBlank(this.employeeSignIn.password)) //THIS WILL ONLY WORK IF ID IS ONLY NUMBERS
+			throw new UnprocessableEntityException("Password");
+		
+	}
+	
+	@Transactional
+	private void updateActiveEmployee() {
+		//Query active user table using session key from request
+		final Optional<ActiveUserEntity> activeUserEntity =
+				this.activeUserRepository.findBySessionKey(this.sessionKey);
+		
+		if(activeUserEntity.isPresent()) { //If record exists, update session key and insert record
+			activeUserEntity.get().setSessionKey(this.sessionKey);
+			this.activeUserRepository.save(activeUserEntity.get());
+		} 
+		else { //If record doesn't exist, set all fields and insert record
+			activeUserEntity.get().setSessionKey(this.sessionKey);
+			activeUserEntity.get().setEmployeeId(this.employeeSignIn.employeeId);
+			activeUserEntity.get().setPassword(this.employeeSignIn.password);
+			this.activeUserRepository.save(activeUserEntity.get());
+		}
+	}
+	
+
+	// Properties
+	private String sessionKey;
+	
+	public String getSessionKey() {
+		return this.sessionKey;
+	}
+	
+	public EmployeeSignInCommand setSessionKey(final String sessionKey){
+		this.sessionKey = sessionKey;
+		return this;
+	}
+	
+	private EmployeeSignIn employeeSignIn;
+	
+	public EmployeeSignInCommand getEmployeeSignIn() {
+		return this.getEmployeeSignIn();
+	}
+	
+	public EmployeeSignInCommand setEmployeeSignIn(final EmployeeSignIn employeeSignIn) {
+		this.employeeSignIn = employeeSignIn;
+		return this;
+	}
+	
+		
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Autowired
+	private ActiveUserRepository activeUserRepository;
+}
