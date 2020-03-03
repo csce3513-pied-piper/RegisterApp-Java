edf8b4919c50a0519fe2dda2efba8acb56500a8b
dubasiy <yrdubasi@uark.edu>
2020-03-03 13:24:04 -0600

    testing

--- src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = {"/", "/mainMenu"})
+@RequestMapping(value = {"/mainMenu"})
 public class MainMenuRouteController {
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView start() {
