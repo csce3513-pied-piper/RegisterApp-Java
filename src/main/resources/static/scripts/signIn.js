4e2d53ca2f062efea3c671d1f1570e38e560da6b
thewa <wjdunn@uark.edu>
2020-03-02 20:53:16 -0600

    Adding JS file from main for testing

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -3,6 +3,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 function validateForm() {
-	// TODO: Validate the user input
-	return true;
+	var number = document.getElementById("ID");
+	var password = document.getElementById("password");
+	
+	//Check for a blank password
+	var numbers = /[0-9]/g;
+	if(number != '' && password != ''){
+		//Check for a numeric ID
+		if(number.match(numbers))
+			return true;
+		else{
+			alert("ID must be numeric!");
+		}
+	}
+		
+	else{
+		alert("Please enter all credentials!");
+		return false;
+	}
 }
