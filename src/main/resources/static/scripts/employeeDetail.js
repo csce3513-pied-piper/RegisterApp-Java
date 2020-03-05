b61ed3819dfc77e8e79d4fbe046926bfe93a7a9a
zhw82 <zhw82@LAPTOP-72OL1H60>
2020-03-05 12:54:15 -0600

    modify HTML and JS

--- src/main/resources/static/scripts/employeeDetail.js
+++ src/main/resources/static/scripts/employeeDetail.js
@@ -7,35 +7,65 @@ document.addEventListener("DOMContentLoaded", () => {
 // Save
 function saveActionClick(event) {
 	// TODO: Actually save the employee via an AJAX call
-	var firstName = document.getElementById("firstname").value;
-	var lastName = document.getElementById("lastname").value;
-	var pass = document.getElementById("password").value;
-	var confirmPass = document.getElementById("cpassword").value;
-	var employeeType = document.getElementById("type").value;
-	
-	if(firstName == "") {
+	if(getFirstName() == "") {
 		alert("Need First Name");
 		return;
 	}
-	else if(lastName == "") {
+	else if(getLastName() == "") {
 		alert("Need Last Name");
 		return;
 	}
-	else if(pass == "") {
+	else if(getPassword() == "") {
 		alert("Need Password");
 		return;
 	}
-	else if(pass != confirmPass) {
+	else if(getPassword() != getCPassword()) {
 		alert("Password and Confirm Password need to be the same");
 		return;
 	}
-	else if(employeeType != "cashier" && employeeType != "shiftmanager" && employeeType != "generalmanager") {
+	else if(getType() != "cashier" && getType() != "shiftmanager" && getType() != "generalmanager") {
 		alert("Employee Type need to be one of the following: Cashier, Shift Manager, General Manager");
 		return;
 	}
+	
+	const saveActionElement = event.target;
+	saveActionElement.disabled = true;
+
+	const firstName = getFirstName();
+	const firstNameIsDefined = (firstName != null);
+	const saveActionUrl = ("/api/employee/"
+		+ (firstNameIsDefined ? firstName : ""));
+	const saveProductRequest = {
+		firstName: getFirstName(),
+		lastName: getLastName(),
+		password: getPassword()
+	};
+
+	if (firstNameIsDefined) {
+		ajaxPut(saveActionUrl, saveProductRequest, (callbackResponse) => {
+			saveActionElement.disabled = false;
+
+			if (isSuccessResponse(callbackResponse)) {
+				displayEmploSavedAlertModal();
+			}
+		});
+	} else {
+		ajaxPost(saveActionUrl, saveProductRequest, (callbackResponse) => {
+			saveActionElement.disabled = false;
+
+			if (isSuccessResponse(callbackResponse)) {
+				displayEmploSavedAlertModal();
+
+				if ((callbackResponse.data != null)) {
+
+					document.getElementById("deleteActionContainer").classList.remove("hidden");
+
+					setFirstName(callbackResponse.data);
+				}
+			}
+		});
+	}
 	displayEmployeeSavedAlertModal();
-	ajaxPatch(resourceRelativeUri, data, callback);
-	ajaxPost(resourceRelativeUri, data, callback);
 }
 
 function displayEmployeeSavedAlertModal() {
@@ -66,5 +96,43 @@ function getSaveActionElement() {
 
 function getSavedAlertModalElement() {
 	return document.getElementById("employeeSavedAlertModal");
+}
+function getDeleteActionElement() {
+	return document.getElementById("deleteButton");
+}
+
+function getFirstName() {
+	return document.getElementById("firstname").value;
+}
+function setFirstName(firstName) {
+	document.getElementById("firstname").value = firstName;
+}
+
+function getLastName() {
+	return document.getElementById("lastname").value;
+}
+function setLastName(lastName) {
+	document.getElementById("lastname").value = lastName;
+}
+
+function getPassword() {
+	return document.getElementById("password").value;
+}
+function setPassword(pass) {
+	document.getElementById("password").value = pass;
+}
+
+function getCPassword() {
+	return document.getElementById("cpassword").value;
+}
+function setCPassword(cpass) {
+	document.getElementById("cpassword").value = cpass;
+}
+
+function getType() {
+	return document.getElementById("type").value;
+}
+function setType(type) {
+	document.getElementById("type").value = type;
 }
 //End getters and setters
