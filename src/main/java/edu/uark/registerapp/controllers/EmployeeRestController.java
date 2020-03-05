d7486f9f9698723cfa6f3d7e131da67b2e7625c6
zhw82 <zhw82@LAPTOP-72OL1H60>
2020-03-04 23:20:03 -0600

    EmployeeRestController

--- src/main/java/edu/uark/registerapp/controllers/EmployeeRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/EmployeeRestController.java
@@ -6,14 +6,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery;
+import edu.uark.registerapp.commands.employees.EmployeeUpdateCommand;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.controllers.enums.QueryParameterNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
@@ -42,7 +45,9 @@ public class EmployeeRestController extends BaseRestController {
 				this.redirectUserNotElevated(request, response);
 		} catch (final NotFoundException e) {
 			isInitialEmployee = true;
+			response.setStatus(HttpServletResponse.SC_FOUND);
 			canCreateEmployeeResponse = new ApiResponse();
+			canCreateEmployeeResponse.setRedirectUrl("/");
 		}
 
 		if (!canCreateEmployeeResponse.getRedirectUrl().equals(StringUtils.EMPTY)) {
@@ -52,14 +57,14 @@ public class EmployeeRestController extends BaseRestController {
 		// TODO: Create an employee;
 		final Employee createdEmployee = new Employee();
 
-		/*if (isInitialEmployee) {
+		if (isInitialEmployee) {
 			createdEmployee
 				.setRedirectUrl(
 					ViewNames.SIGN_IN.getRoute().concat(
 						this.buildInitialQueryParameter(
-							QueryParameterNames.EMPLOYEE_ID.getValue(),
+							"employeeId",//QueryParameterNames.EMPLOYEE_ID.getValue(),
 							createdEmployee.getEmployeeId())));
-		}*/
+		}
 
 		return createdEmployee.setIsInitialEmployee(isInitialEmployee);
 	}
@@ -74,11 +79,25 @@ public class EmployeeRestController extends BaseRestController {
 
 		final ApiResponse elevatedUserResponse =
 			this.redirectUserNotElevated(request, response);
+		ApiResponse canCreateEmployeeResponse;
+		ActiveEmployeeExistsQuery activeUser = new ActiveEmployeeExistsQuery();
+
+		try {
+			// TODO: Query if any active employees exist
+			activeUser.execute();
+		} catch (final NotFoundException e) {
+			response.setStatus(HttpServletResponse.SC_FOUND);
+			canCreateEmployeeResponse =
+					this.redirectUserNotElevated(request, response);
+		}
 		if (!elevatedUserResponse.getRedirectUrl().equals(StringUtils.EMPTY)) {
 			return elevatedUserResponse;
 		}
 
+		employeeUpdate.execute();
 		// TODO: Update the employee
 		return employee;
 	}
+	@Autowired
+    private EmployeeUpdateCommand employeeUpdate;
 }
