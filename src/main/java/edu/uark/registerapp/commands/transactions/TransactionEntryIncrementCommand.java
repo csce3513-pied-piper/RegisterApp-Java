8209229eb51066840a9e01b1d9568603099b24d8
dubasiy <yrdubasi@uark.edu>
2020-04-11 11:40:39 -0500

    fixed TransactionEntryIncrementCommand

--- src/main/java/edu/uark/registerapp/commands/transactions/TransactionEntryIncrementCommand.java
+++ src/main/java/edu/uark/registerapp/commands/transactions/TransactionEntryIncrementCommand.java
@@ -1,61 +1,47 @@
 package edu.uark.registerapp.commands.transactions;
 
-package edu.uark.registerapp.commands.transactions;
-
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import javax.transaction.Transactional;
-
-import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import edu.uark.registerapp.commands.ResultCommandInterface;
-import edu.uark.registerapp.commands.exceptions.ConflictException;
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
-import edu.uark.registerapp.models.api.Product;
 import edu.uark.registerapp.models.entities.TransactionEntryEntity;
 import edu.uark.registerapp.models.repositories.TransactionEntryRepository;
 
 @Service
-public class TransactionEntryIncrementCommand implements ResultCommandInterface<Product>{
-	@Override
-    public Product execute() {
-        final TransactionEntryEntity updatedTransactionEntryEntity = this.incrementTransactionEntryEntity();
+public class TransactionEntryIncrementCommand implements VoidCommandInterface {
+    @Transactional
+    @Override
+    public void execute() {
+
+        final Optional<TransactionEntryEntity> transactionEntryEntity =
+                this.transactionEntryRepository.findById(this.transactionEntryId);
+        if (!transactionEntryEntity.isPresent()) { // No record with the associated record ID exists in the database.
+            throw new NotFoundException("Product");
+        }
+        double current = transactionEntryEntity.get().getQuantity();
+        transactionEntryEntity.get().setQuantity(current + 1);
+
+        // Write, via an UPDATE, any changes to the database.
+        this.transactionEntryRepository.save(transactionEntryEntity.get());
     }
 
-@Transactional
-public TransactionEntryEntity incrementTransactionEntryEntity() {
-	final Optional<TransactionEntryEntity> transactionEntryEntity =
-            this.transactionEntryRepository.findById(this.productId);
-	
-	if (!transactionEntryEntity.isPresent()) { // No record with the associated record ID exists in the database.
-        throw new NotFoundException("TransactionEntry");
+    // Properties
+    private UUID transactionEntryId;
+    public UUID getTransactionEntryId() {
+        return this.transactionEntryId;
     }
-	
-	//Get quantity and increment by one
-	double newQuantity = transactionEntryEntity.get().getQuantity() + 1;
-	transactionEntryEntity.get().setQuantity(newQuantity);
-	
-	// Write, via an UPDATE, any changes to the database.
-	this.transactionEntryRepository.save(transactionEntryEntity.get());
+    public TransactionEntryIncrementCommand setProductId(final UUID transactionEntryId) {
+        this.transactionEntryId = transactionEntryId;
+        return this;
+    }
 
-}
-
-
-//Properties
-private UUID productId;
-public UUID getProductId() {
-    return this.productId;
-}
-public TransactionEntryIncrementCommand setProductId(final UUID productId) {
-    this.productId = productId;
-    return this;
-}
-
-@Autowired
-private TransactionEntryRepository transactionEntryRepository;
+    @Autowired
+    private TransactionEntryRepository transactionEntryRepository;
 }
