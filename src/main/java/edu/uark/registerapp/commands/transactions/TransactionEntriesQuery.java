51d04c8faaa388c57679c2a65bb051cf151dc00a
dubasiy <yrdubasi@uark.edu>
2020-04-11 11:06:34 -0500

    A lot of work, to get stuff on the transaction menu page

---
+++ src/main/java/edu/uark/registerapp/commands/transactions/TransactionEntriesQuery.java
@@ -0,0 +1,47 @@
+package edu.uark.registerapp.commands.transactions;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import javafx.util.Pair;
+import java.util.UUID;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ProductEntity;
+import edu.uark.registerapp.models.repositories.ProductRepository;
+import edu.uark.registerapp.models.entities.TransactionEntryEntity;
+import edu.uark.registerapp.models.repositories.TransactionEntryRepository;
+
+@Service
+public class TransactionEntriesQuery implements ResultCommandInterface<Pair<List<TransactionEntryEntity>, List<Product>>> {
+    @Override
+    public Pair<List<TransactionEntryEntity>, List<Product>> execute() {
+        final LinkedList<TransactionEntryEntity> transactionEntryEntities = new LinkedList<TransactionEntryEntity>();
+        final LinkedList<Product> products = new LinkedList<Product>();
+
+        for (final TransactionEntryEntity transactionEntryEntity : transactionEntryRepository.findAll()) {
+            transactionEntryEntities.addLast(transactionEntryEntity);
+            UUID pId = transactionEntryEntity.getProductId();
+            final Optional<ProductEntity> productEntity = productRepository.findById(pId);
+            if (productEntity.isPresent()) {
+                products.addLast(new Product(productEntity.get()));
+            } else {
+                throw new NotFoundException("Product");
+            }
+        }
+
+        return new Pair<List<TransactionEntryEntity>, List<Product>>(transactionEntryEntities, products);
+    }
+
+    @Autowired
+    TransactionEntryRepository transactionEntryRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+}
